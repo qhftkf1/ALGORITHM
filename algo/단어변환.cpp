c00a16df7ab919798206fdca921c1c51490eb274
Evan Song <qhftkf1@naver.com>
2020-08-18 17:10:40 +0900

    0818_튜플

--- algo/단어변환.cpp
+++ algo/단어변환.cpp
@@ -1,44 +1,49 @@
-#include <string>
-#include <vector>
-#include <bits/stdc++.h>
-
-using namespace std;
-vector<int> check(51);
-int cnt = 0;
-int min_value = 2e9;
-int fn_compare(string a, string b) {
-    int count = 0;
-    int i = 0;
-    while (i < a.length()) {
-        if (a[i] != b[i])
-            count++;
-        i++;
-    }
-    return count;
-}
-
-int solution(string begin, string target, vector<string> words) {
-    if (find(words.begin(), words.end(), target) == words.end())
-        return 0;
-    if (begin == target) {
-        if (min_value > cnt)
-            min_value = cnt;
-    }
-    for (int i = 0; i < words.size(); i++) {
-        if (fn_compare(begin, words[i]) == 1 && check[i] == 0) {
-            check[i] = 1; cnt++;
-            solution(words[i], target, words);
-            check[i] = 0; cnt--;
-        }
-    }
-    return min_value;
-}
-
-int main(void) {
-    string begin = "hit";
-    string target = "coo";
-
-    vector<string> words = { "hot", "dot", "dog", "lot", "log", "cog" };
-
-    cout << solution(begin, target, words);
-}
+//#include <string>
+//#include <vector>
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//vector<int> check(51);
+//int cnt = 0;
+//int min_value = 2e9;
+//int fn_compare(string a, string b) {
+//    int count = 0;
+//    int i = 0;
+//    while (i < a.length()) {
+//        if (a[i] != b[i])
+//            count++;
+//        i++;
+//    }
+//    return count == 1;
+//}
+//
+//void dfs(string& begin, string& target, int cnt, vector<string>& words) {
+//    if (begin == target) {
+//        if (min_value > cnt)
+//            min_value = cnt;
+//    }
+//    for (int i = 0; i < words.size(); i++) {
+//        if (fn_compare(begin, words[i]) && check[i] == 0) {
+//            check[i] = 1; cnt++;
+//            solution(words[i], target, words);
+//            check[i] = 0; cnt--;
+//        }
+//    }
+//}
+//
+//int solution(string begin, string target, vector<string> words) {
+//    
+//    if (min_value == (int)2e9)
+//        min_value = 0;
+//    return min_value;
+//}
+//
+//int main(void) {
+//    string begin = "hit";
+//    string target = "coo";
+//
+//    vector<string> words = { "hot", "dot", "dog", "lot", "log", "cog" };
+//
+//    
+//    cout << solution(begin, target, words);
+//}
---
+++ algo/튜플.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    vector<string> tuple_s;
+    set<int> c_n;
+    map<int, int> ma;
+    string test;
+    istringstream ss(s.substr(1, s.size() - 2));
+
+    while (getline(ss, test, '{')) {
+        getline(ss, test, '}');
+        tuple_s.push_back(test);
+    }
+    for (int i = 0; i < tuple_s.size(); i++) {
+        istringstream ss(tuple_s[i]);
+        while (getline(ss, test, ',')) {
+            ma[stoi(test)]++;
+            c_n.insert(stoi(test));
+        }
+    }
+    for (int i = 0; i < ma.size(); i++) {
+        answer.push_back(0);
+    }
+    while (!(c_n.empty())) {
+        answer[ma.size() - ma[*c_n.begin()]] = *c_n.begin();
+        c_n.erase(c_n.begin());
+    }
+    return answer;
+}
+
+int main(void) {
+    string s = "{ {2}, { 2,123 }, { 2,123,3 }, { 2,123,3,4 }}";
+    solution(s);
+}
