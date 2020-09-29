6989ac15f6e2a050a6db7280421341f948045eb1
Evan Song <qhftkf1@naver.com>
2020-09-29 22:00:31 +0900

    0929_야구

--- algo/테트로미노.cpp
+++ algo/테트로미노.cpp
@@ -1,61 +1,61 @@
-//#include<bits/stdc++.h>
-//
-//using namespace std;
-//
-//int main(void) {
-//
-//	int N, M;
-//	cin >> N >> M;
-//	vector<vector<int>> arr(N, vector<int>(M));
-//	for (int i = 0; i < N; i++) {
-//		for (int j = 0; j < M; j++) {
-//			int a;
-//			cin >> a;
-//			arr[i][j] = a;
-//		}
-//	}
-//	vector<int> max_list;
-//	for (int i = 0; i < N; i++) {
-//		for (int j = 0; j < M; j++) {
-//			if (j + 3 < M)
-//				max_list.push_back(arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i][j + 3]);
-//			if (i + 3 < N)
-//				max_list.push_back(arr[i][j] + arr[i + 1][j] + arr[i + 2][j] + arr[i + 3][j]);
-//			if (i + 1 < N && j + 1 < M)
-//				max_list.push_back(arr[i][j] + arr[i + 1][j] + arr[i][j + 1] + arr[i + 1][j + 1]);
-//			
-//			if (i + 2 < N && j + 1 < M) {
-//				int square = arr[i][j] + arr[i][j + 1] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1];
-//
-//				max_list.push_back(square - arr[i][j + 1] - arr[i + 1][j + 1]);
-//				max_list.push_back(square - arr[i][j] - arr[i + 1][j]);
-//
-//				max_list.push_back(square - arr[i + 1][j] - arr[i + 2][j]);
-//				max_list.push_back(square - arr[i + 1][j + 1] - arr[i + 2][j + 1]);
-//
-//				max_list.push_back(square - arr[i][j + 1] - arr[i + 2][j]);
-//				max_list.push_back(square - arr[i][j] - arr[i + 2][j + 1]);
-//
-//				max_list.push_back(square - arr[i][j] - arr[i + 2][j]);
-//				max_list.push_back(square - arr[i][j + 1] - arr[i + 2][j + 1]);
-//			}
-//			if (i + 1 < N && j + 2 < M) {
-//				int square = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 1][j + 2];
-//
-//				max_list.push_back(square - arr[i + 1][j + 1] - arr[i + 1][j + 2]);
-//				max_list.push_back(square - arr[i][j + 1] - arr[i][j + 2]);
-//
-//				max_list.push_back(square - arr[i][j] - arr[i][j + 1]);
-//				max_list.push_back(square - arr[i + 1][j] - arr[i + 1][j + 1]);
-//
-//				max_list.push_back(square - arr[i][j] - arr[i + 1][j + 2]);
-//				max_list.push_back(square - arr[i + 1][j] - arr[i][j + 2]);
-//
-//				max_list.push_back(square - arr[i + 1][j] - arr[i + 1][j + 2]);
-//				max_list.push_back(square - arr[i][j] - arr[i][j + 2]);
-//			}
-//		}
-//	}
-//	int ans = *max_element(max_list.begin(), max_list.end());
-//	cout << ans << "\n";
-//}
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+
+	int N, M;
+	cin >> N >> M;
+	vector<vector<int>> arr(N, vector<int>(M));
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int a;
+			cin >> a;
+			arr[i][j] = a;
+		}
+	}
+	vector<int> max_list;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (j + 3 < M)
+				max_list.push_back(arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i][j + 3]);
+			if (i + 3 < N)
+				max_list.push_back(arr[i][j] + arr[i + 1][j] + arr[i + 2][j] + arr[i + 3][j]);
+			if (i + 1 < N && j + 1 < M)
+				max_list.push_back(arr[i][j] + arr[i + 1][j] + arr[i][j + 1] + arr[i + 1][j + 1]);
+			
+			if (i + 2 < N && j + 1 < M) {
+				int square = arr[i][j] + arr[i][j + 1] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1];
+
+				max_list.push_back(square - arr[i][j + 1] - arr[i + 1][j + 1]);
+				max_list.push_back(square - arr[i][j] - arr[i + 1][j]);
+
+				max_list.push_back(square - arr[i + 1][j] - arr[i + 2][j]);
+				max_list.push_back(square - arr[i + 1][j + 1] - arr[i + 2][j + 1]);
+
+				max_list.push_back(square - arr[i][j + 1] - arr[i + 2][j]);
+				max_list.push_back(square - arr[i][j] - arr[i + 2][j + 1]);
+
+				max_list.push_back(square - arr[i][j] - arr[i + 2][j]);
+				max_list.push_back(square - arr[i][j + 1] - arr[i + 2][j + 1]);
+			}
+			if (i + 1 < N && j + 2 < M) {
+				int square = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 1][j + 2];
+
+				max_list.push_back(square - arr[i + 1][j + 1] - arr[i + 1][j + 2]);
+				max_list.push_back(square - arr[i][j + 1] - arr[i][j + 2]);
+
+				max_list.push_back(square - arr[i][j] - arr[i][j + 1]);
+				max_list.push_back(square - arr[i + 1][j] - arr[i + 1][j + 1]);
+
+				max_list.push_back(square - arr[i][j] - arr[i + 1][j + 2]);
+				max_list.push_back(square - arr[i + 1][j] - arr[i][j + 2]);
+
+				max_list.push_back(square - arr[i + 1][j] - arr[i + 1][j + 2]);
+				max_list.push_back(square - arr[i][j] - arr[i][j + 2]);
+			}
+		}
+	}
+	int ans = *max_element(max_list.begin(), max_list.end());
+	cout << ans << "\n";
+}
--- algo/회문1.cpp
+++ algo/회문1.cpp
@@ -1,72 +1,72 @@
-//#include<bits/stdc++.h>
-//
-//using namespace std;
-//int res;
-//vector<vector<int>> arr(8, vector<int>(8));
-//void right_check(int x, int y,int n){
-//	int left, right;
-//	left = x;
-//	right = x + n - 1;
-//	while (left <= right) {
-//		if (arr[y][left] != arr[y][right])
-//			break;
-//		left++;
-//		right--;
-//	}
-//	if (left > right)
-//		res++;
-//}
-//
-//void down_check(int x, int y, int n) {
-//	int down, up;
-//	down = y;
-//	up = y + n - 1;
-//	while (down <= up) {
-//		if (arr[down][x] != arr[up][x])
-//			break;
-//		down++;
-//		up--;
-//	}
-//	if (down > up)
-//		res++;
-//}
-//int main(int argc, char** argv)
-//{
-//	int test_case;
-//	int T;
-//	
-//	T = 10;
-//	
-//	for (test_case = 1; test_case <= T; ++test_case)
-//	{
-//		int n;
-//		char input;
-//		res = 0;
-//		cin >> n;
-//		for (int i = 0; i < 8; i++) {
-//			for (int j = 0; j < 8; j++) {
-//				cin >> input;
-//				arr[i][j] = input;
-//			}
-//		}
-//		for (int i = 0; i < 8; i++) {
-//			for (int j = 0; j < 8; j++) {
-//				if (i + n - 1 < 8) {
-//					down_check(j, i, n);
-//				}
-//				if (j + n - 1 < 8) {
-//					right_check(j, i, n);
-//				}
-//			}
-//		}
-//		cout << "#" << test_case << " " << res << "\n";
-//		/////////////////////////////////////////////////////////////////////////////////////////////
-//		/*
-//			 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
-//		 */
-//		 /////////////////////////////////////////////////////////////////////////////////////////////
-//
-//
-//	}
-//	return 0;//정상종료시 반드시 0을 리턴해야합니다.
-//}
+#include<bits/stdc++.h>
+
+using namespace std;
+int res;
+vector<vector<int>> arr(8, vector<int>(8));
+void right_check(int x, int y,int n){
+	int left, right;
+	left = x;
+	right = x + n - 1;
+	while (left <= right) {
+		if (arr[y][left] != arr[y][right])
+			break;
+		left++;
+		right--;
+	}
+	if (left > right)
+		res++;
+}
+
+void down_check(int x, int y, int n) {
+	int down, up;
+	down = y;
+	up = y + n - 1;
+	while (down <= up) {
+		if (arr[down][x] != arr[up][x])
+			break;
+		down++;
+		up--;
+	}
+	if (down > up)
+		res++;
+}
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	
+	T = 10;
+	
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int n;
+		char input;
+		res = 0;
+		cin >> n;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				cin >> input;
+				arr[i][j] = input;
+			}
+		}
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (i + n - 1 < 8) {
+					down_check(j, i, n);
+				}
+				if (j + n - 1 < 8) {
+					right_check(j, i, n);
+				}
+			}
+		}
+		cout << "#" << test_case << " " << res << "\n";
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/*
+			 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+		 */
+		 /////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
