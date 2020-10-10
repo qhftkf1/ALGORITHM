8bee3a93a949ed9b021d94ce9e829564f48eedf5
Evan Song <qhftkf1@naver.com>
2020-10-10 11:52:53 +0900

    20201010_그대로 출력하기

--- algo/그대로출력하기.cpp
+++ algo/그대로출력하기.cpp
@@ -3,21 +3,8 @@
 using namespace std;
 int main(void)
 {
-	char s[10001];
-	char input;
-
-	int i = 0;
-	int flag;
-	flag = 0;
-	while (1) 
-	{
-		scanf_s("%1c", &input);
-		s[i] = input;
-		if (input == '\n' && flag == 1)
-			break;
-		else if (input == ' ' && flag == 0)
-			flag = 1;
-		else flag = 0;
-		i++;
-	}
+	string s;
+	while (getline(cin,s)) 
+		cout << s << "\n";
+	
 }
